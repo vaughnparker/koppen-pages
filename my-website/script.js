ad88167db1d563510594f9dc77c5ae289640b514
vaughnparker <vaughnjparker@gmail.com>
2025-10-06 20:52:12 -0700

    Moves files to one folder

---
+++ my-website/script.js
@@ -0,0 +1,4 @@
+document.getElementById("clickMe").addEventListener("click", function() {
+  const output = document.getElementById("output");
+  output.textContent = "You clicked the button! 🎉";
+});
--- my_website/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("clickMe").addEventListener("click", function() {
-  const output = document.getElementById("output");
-  output.textContent = "You clicked the button! 🎉";
-});
